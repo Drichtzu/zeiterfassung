4b2bdcea963e4f92fcfdcc9400f00008abe8bc7c
Drichtzu <drichtzu@gmail.com>
2024-07-18 20:49:05 +0200

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,20 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const loginButton = document.getElementById('loginButton');
-    loginButton.addEventListener('click', showLoginPrompt);
+    if (loginButton) {
+        loginButton.addEventListener('click', showLoginPrompt);
+    } else {
+        console.error('Login-Button nicht gefunden');
+    }
 });
 
 function showLoginPrompt() {
     const userInput = prompt("Bitte geben Sie das Admin-Passwort ein:");
 
     if (userInput !== null) {
-        fetch('/api/admin-login.php', {
+        fetch('http://192.168.177.159/api/admin-login.php', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ password: userInput })
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Netzwerkantwort war nicht ok');
+            }
+            return response.json();
+        })
         .then(data => {
             if (data.success) {
                 sessionStorage.setItem('isAdmin', 'true');
