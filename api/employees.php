68b53bc5738d57aef090c0bdb1964d5655518b5d
Drichtzu <drichtzu@gmail.com>
2024-07-18 21:47:15 +0200

    Update employees.php

--- api/employees.php
+++ api/employees.php
@@ -2,13 +2,6 @@
 require_once 'config.php';
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    exit(0);
-}
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     try {
