9d1ad230090934d4f5738afafd935b8e2d24a587
Drichtzu <drichtzu@gmail.com>
2024-07-18 21:18:17 +0200

    Delete api directory

--- api/db_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'config.php';
-
-try {
-    $pdo->query('SELECT 1');
-    echo "Datenbankverbindung erfolgreich!";
-} catch (PDOException $e) {
-    echo "Fehler bei der Datenbankverbindung: " . $e->getMessage();
-}
